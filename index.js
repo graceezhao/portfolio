b39aaf2502aa987e845cdfaf2caae6302d3db8b0
graceezhao <71144509+graceezhao@users.noreply.github.com>
2023-07-28 17:09:41 -0500

    initial commit

--- index.js
+++ index.js
@@ -37,23 +37,27 @@ headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
 
-function FadeInSection(props) {
-  const [isVisible, setVisible] = React.useState(true);
-  const domRef = React.useRef();
-  React.useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => setVisible(entry.isIntersecting));
-    });
-    observer.observe(domRef.current);
-    return () => observer.unobserve(domRef.current);
-  }, []);
-  return (
-    <div
-      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-      ref={domRef}
-    >
-      {props.children}
-    </div>
-  );
-}
+// function FadeInSection(props) {
+//   const [isVisible, setVisible] = React.useState(true);
+//   const domRef = React.useRef();
+//   React.useEffect(() => {
+//     const observer = new IntersectionObserver(entries => {
+//       entries.forEach(entry => setVisible(entry.isIntersecting));
+//     });
+//     observer.observe(domRef.current);
+//     return () => observer.unobserve(domRef.current);
+//   }, []);
+//   return (
+//     <div
+//       className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+//       ref={domRef}
+//     >
+//       {props.children}
+//     </div>
+//   );
+// }
+
+// $('input').on('change', function() {
+//   $('body').toggleClass('blue');
+// });
 
